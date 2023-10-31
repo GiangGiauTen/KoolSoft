99f737fce7f1963d6c679654bf9f6f6945105e71
River <letruonggiang288@gmail.com>
2023-11-01 00:13:12 +0700

    Count

--- src/components/Swipper/SwiperImage.js
+++ src/components/Swipper/SwiperImage.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import { Carousel } from 'antd';
 import './SwiperImage.css';
-const contentStyle = {
-  height: '160px',
-  color: '#fff',
-  lineHeight: '160px',
-  textAlign: 'center',
-  background: '#364d79',
-};
+
 const images = [
   'slide1.png',
   'slide2.png',
@@ -22,12 +16,8 @@ function SwiperImage() {
   return (
     <Carousel autoplay customPaging={i => <div className="custom-dot"></div>}>
       {images.map((image, index) => (
-        <div key={index}>
-          <img
-            src={`/slides/${image}`}
-            alt={`Slide ${index + 1}`}
-            style={contentStyle}
-          />
+        <div key={index} className="slide">
+          <img src={`/slides/${image}`} alt={`Slide ${index + 1}`} />
         </div>
       ))}
     </Carousel>
